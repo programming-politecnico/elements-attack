19858e96027b79624927a3dbe71a10f3fcd4cee2
lpatronr <lpatronr@gmail.com>
2022-11-22 16:56:07 -0500

    feat: add button styles

--- src/main/java/com/poli/elementsattack/HelloController.java
+++ src/main/java/com/poli/elementsattack/HelloController.java
@@ -1,14 +1,10 @@
 package com.poli.elementsattack;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void startGameHandler() {
+        System.out.println("Start Game");
     }
 }
