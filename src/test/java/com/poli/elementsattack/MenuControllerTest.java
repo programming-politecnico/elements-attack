f34144234cbefb0349763a903cecd97c8d5d8eb7
lpatronr <lpatronr@gmail.com>
2022-11-22 17:22:12 -0500

    feat: load new scene on click

--- src/test/java/com/poli/elementsattack/MenuControllerTest.java
+++ src/test/java/com/poli/elementsattack/MenuControllerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class HelloControllerTest {
+class MenuControllerTest {
 
     @Test
     void onHelloButtonClick() {
